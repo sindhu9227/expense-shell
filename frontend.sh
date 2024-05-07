4db410d124f314b9624659adb411bef77dc3340d
sindhu <sindhudevops27@gmail.com>
2024-05-08 00:05:09 +0530

    Updated frontend script with print statements

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,28 @@
-dnf install nginx -y
+source common.sh
+rm -f /tmp/expense.log
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+HEADING Installing Nginx
+dnf install nginx -y &>>/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+HEADING Copy expense config file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
+STAT $?
+
+HEADING clean old content
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+STAT $?
+
+HEADING download frontend content
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
+STAT $?
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
-#Copy step
+HEADING extract content
+unzip /tmp/frontend.zip &>>/tmp/expense.log
+STAT $?
 
-systemctl restart nginx
-systemctl enable nginx
+HEADING restart service
+systemctl enable nginx &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
+STAT $?

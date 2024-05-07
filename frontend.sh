27c0d191330af2348af6ced4b9011944473a7581
sindhu <sindhudevops27@gmail.com>
2024-05-07 23:38:01 +0530

    Added mysql scripts

--- frontend.sh
+++ frontend.sh
@@ -2,9 +2,6 @@ dnf install nginx -y
 
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-systemctl enable nginx
-systemctl start nginx
-
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 
@@ -14,3 +11,4 @@ unzip /tmp/frontend.zip
 #Copy step
 
 systemctl restart nginx
+systemctl enable nginx
